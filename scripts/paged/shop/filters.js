1ded276d7fc58bf8fa66167cffefe807ea7c9cf9
saba <sabagochishvili@gmail.com>
2026-09-22 10:51:37 +0400

    add rating and min-max price filters and make them work

--- scripts/paged/shop/filters.js
+++ scripts/paged/shop/filters.js
@@ -2,11 +2,19 @@ import { fetchCategories } from "../../data/categories.js";
 import { filterState } from "../../data/filterData.js";
 import { renderProducts } from "./shop.js";
 
+//categories
 const categoriesElement = document.querySelector(".categories");
+const minRatingOptions = document.querySelectorAll(".rating-option");
+const minPriceInput = document.querySelector(".min-price-input");
+const maxPriceInput = document.querySelector(".max-price-input");
+
 async function renderFilters() {
   const categories = await fetchCategories();
   renderCategories(categories);
   handleCategoryController();
+  handleMinRatingController();
+  handleMinPriceController();
+  handleMaxPriceController();
 }
 renderFilters();
 
@@ -31,5 +39,38 @@ function handleCategoryController() {
       filterState.categoryId = Number(categoryId);
       renderProducts();
     });
+  });
+}
+
+//star ratings
+
+function handleMinRatingController() {
+  minRatingOptions.forEach((option) => {
+    const minRating = option.dataset.minRating;
+
+    option.addEventListener("click", () => {
+      filterState.minRating = minRating;
+      renderProducts();
+    });
+  });
+}
+
+function handleMinPriceController() {
+  minPriceInput.addEventListener("input", (e) => {
+    let inputValue = Number(e.target.value);
+    if (typeof inputValue === "number") {
+      filterState.minPrice = e.target.value;
+      renderProducts();
+    }
+  });
+}
+
+function handleMaxPriceController() {
+  maxPriceInput.addEventListener("input", (e) => {
+    let inputValue = Number(e.target.value);
+    if (typeof inputValue === "number") {
+      filterState.maxPrice = e.target.value;
+      renderProducts();
+    }
   });
 }
